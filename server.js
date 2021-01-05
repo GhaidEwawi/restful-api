0f08292c3dc944ffabcbcab694034547ed9d9de2
GhaidEwawi <ghaid_99@hotmail.com>
2021-01-05 16:14:42 +0200

    connect to database

--- server.js
+++ server.js
@@ -2,4 +2,15 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const app = express();
 
+// Database
+mongoose.connect('mongodb://127.0.0.1:27017/user-manager', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('Connected to database...'))
+    .catch(err => console.error(err))
 
+// Middleware
+
+// Controllers
+
+// Routes
+
+// Start Server
